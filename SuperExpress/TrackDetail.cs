73bb25806caa00cc737ab11e5f6157dbb3caf214
Jing Wang <wjing.ca@gmail.com>
2018-03-28 09:18:14 -0400

    * commit by jing

---
+++ SuperExpress/TrackDetail.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperExpress
+{
+    class TrackDetail
+    {
+        public string City { get; set; }
+        public string StateProvinceCode { get; set; }
+        public string CountryCode { get; set; }
+        public string PostalCode { get; set; }
+        public DateTime OccurredAt { get; set; }
+        public string Activity { get; set; }
+
+        public string Location
+        {
+            get
+            {
+                List<string> locList = new List<string>();
+                if (!City.Equals(""))
+                {
+                    locList.Add(City);
+                }
+
+                if (!StateProvinceCode.Equals(""))
+                {
+                    locList.Add(StateProvinceCode);
+                }
+
+                if (!CountryCode.Equals(""))
+                {
+                    locList.Add(CountryCode);
+                }
+
+                if (!PostalCode.Equals(""))
+                {
+                    locList.Add(PostalCode);
+                }
+
+                return string.Join(",", locList);
+            }
+        }
+
+        public string Date
+        {
+            get
+            {
+                return OccurredAt.ToString("MM/dd/yyyy");
+            }
+        }
+
+        public string LocalTime
+        {
+            get
+            {
+                return OccurredAt.ToString("HH:mm");
+            }
+        }
+    }
+}
