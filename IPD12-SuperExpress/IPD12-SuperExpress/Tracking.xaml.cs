5b8cddd46053a4dc22dc0d755fce0640da2def74
jie zhu <zhujie8088@gmail.com>
2018-03-27 23:52:38 -0400

    merge

--- IPD12-SuperExpress/IPD12-SuperExpress/Tracking.xaml.cs
+++ IPD12-SuperExpress/IPD12-SuperExpress/Tracking.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Maps.MapControl.WPF;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,20 @@ namespace IPD12_SuperExpress
         public Tracking()
         {
             InitializeComponent();
+        }
+
+        private void btnTracking_Click(object sender, RoutedEventArgs e)
+        {
+            MapPolyline polyline = new MapPolyline();
+            polyline.Stroke = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Colors.Blue);
+            polyline.StrokeThickness = 5;
+            polyline.Opacity = 0.7;
+            polyline.Locations = new LocationCollection() {
+        new Location(47.6424, -122.3219),
+        new Location(47.8424,-122.1747),
+        new Location(47.67856,-122.130994)};
+
+            myMap.Children.Add(polyline);
         }
     }
 }
