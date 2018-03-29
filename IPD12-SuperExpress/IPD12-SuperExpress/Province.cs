5cfdbc357308e36c99dea60c4c406a5f777c84b1
Jing Wang <wjing.ca@gmail.com>
2018-03-29 11:40:19 -0400

    * commit by jing @JAC

---
+++ IPD12-SuperExpress/IPD12-SuperExpress/Province.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPD12_SuperExpress
+{
+    public class Province
+    {
+        public string CountryCode { get; set; }
+        public string ProvinceStateCode { get; set; }
+        public string ProvinceStateName { get; set; }
+    }
+}
