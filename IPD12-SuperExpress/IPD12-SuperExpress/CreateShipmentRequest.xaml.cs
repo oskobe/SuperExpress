f0b7dfd9f450ce9746705e19d797c3288ea754f4
Wang, Jing <wjing.ca@gmail.com>
2018-04-04 23:05:41 -0400

    * commit by Jing <Implementing PDF format Shipping Invoice>

--- IPD12-SuperExpress/IPD12-SuperExpress/CreateShipmentRequest.xaml.cs
+++ IPD12-SuperExpress/IPD12-SuperExpress/CreateShipmentRequest.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using Microsoft.Win32;
 using MySql.Data;
 using MySql.Data.MySqlClient;
 using ShipEngine.ApiClient.Api;
@@ -28,6 +32,7 @@ namespace IPD12_SuperExpress
         List<Province> provinceList = new List<Province>();
         CostCalculator costCalculator;
         SuperExpressRate rate;
+        ShipmentRequest shipmentRequest;
 
         public CreateShipmentRequest(CostCalculator cal, SuperExpressRate rate)
         {
@@ -87,8 +92,8 @@ namespace IPD12_SuperExpress
 
 
             /* for test start*/
-            tbAddressFrom1.Text = "";
-            tbAddressTo1.Text = "";
+            tbAddressFrom1.Text = "4100 Rue Goyer";
+            tbAddressTo1.Text = "3-9992 Av Fontenac";
             /* for test end */
 
         }
@@ -122,39 +127,252 @@ namespace IPD12_SuperExpress
                 return;
             }
 
-            ShipmentRequest sr = new ShipmentRequest();
-            sr.ServiceType = rate.ServiceType;
-            sr.GuaranteedService = rate.GuaranteedService;
-            sr.EstimatedDeliveryDate = rate.EstimatedDeliveryDateTime;
-            sr.Weight = costCalculator.Weight.Value??0;
-            sr.WeightUnit = costCalculator.Weight.Unit.ToString();
-            sr.Length = costCalculator.Dimensions.Length??0;
-            sr.Width = costCalculator.Dimensions.Width ?? 0;
-            sr.Height = costCalculator.Dimensions.Height ?? 0;
-            sr.DimensionsUnit = costCalculator.Dimensions.Unit.ToString();
-            sr.Amount = rate.Amount;
-            sr.Currency = Globals.CURRENCY_CAD.Trim();
-            sr.CountryFrom = cbCountryFrom.Text;
-            sr.ProvinceFrom = cbProvinceStateFrom.Text;
-            sr.CityFrom = tbCityFrom.Text;
-            sr.Address1From = tbAddressFrom1.Text;
-            sr.Address2From = tbAddressFrom2.Text;
-            sr.PostalCodeFrom = tbPostalCodeFrom.Text;
-            sr.CountryTo = cbCountryTo.Text;
-            sr.ProvinceTo = cbProvinceStateTo.Text;
-            sr.CityTo = tbCityTo.Text;
-            sr.Address1To = tbAddressTo1.Text;
-            sr.Address2To = tbAddressTo2.Text;
-            sr.PostalCodeTo = tbPostalCodeTo.Text;
+            shipmentRequest = new ShipmentRequest();
+            shipmentRequest.ServiceType = rate.ServiceType;
+            shipmentRequest.GuaranteedService = rate.GuaranteedService;
+            shipmentRequest.EstimatedDeliveryDate = rate.EstimatedDeliveryDateTime;
+            shipmentRequest.Weight = costCalculator.Weight.Value??0;
+            shipmentRequest.WeightUnit = costCalculator.Weight.Unit.ToString();
+            shipmentRequest.Length = costCalculator.Dimensions.Length??0;
+            shipmentRequest.Width = costCalculator.Dimensions.Width ?? 0;
+            shipmentRequest.Height = costCalculator.Dimensions.Height ?? 0;
+            shipmentRequest.DimensionsUnit = costCalculator.Dimensions.Unit.ToString();
+            shipmentRequest.Amount = rate.Amount;
+            shipmentRequest.Currency = Globals.CURRENCY_CAD.Trim();
+            shipmentRequest.CountryFrom = cbCountryFrom.Text;
+            shipmentRequest.ProvinceFrom = cbProvinceStateFrom.Text;
+            shipmentRequest.CityFrom = tbCityFrom.Text;
+            shipmentRequest.Address1From = tbAddressFrom1.Text;
+            shipmentRequest.Address2From = tbAddressFrom2.Text;
+            shipmentRequest.PostalCodeFrom = tbPostalCodeFrom.Text;
+            shipmentRequest.CountryTo = cbCountryTo.Text;
+            shipmentRequest.ProvinceTo = cbProvinceStateTo.Text;
+            shipmentRequest.CityTo = tbCityTo.Text;
+            shipmentRequest.Address1To = tbAddressTo1.Text;
+            shipmentRequest.Address2To = tbAddressTo2.Text;
+            shipmentRequest.PostalCodeTo = tbPostalCodeTo.Text;
 
             try
             {
-                string newGeneratedId = Globals.db.AddOrder(sr);
-                MessageBox.Show("Shipment request " + newGeneratedId + " was created.");
+                string newGeneratedId = Globals.db.AddOrder(shipmentRequest);
+                MessageBoxResult result = MessageBox.Show("Shipment request #" + newGeneratedId + " was created, \nDo you want to save PDF format INVOICE?", "Confirmation", MessageBoxButton.YesNo, MessageBoxImage.Question, MessageBoxResult.No);
+                if (result == MessageBoxResult.Yes)
+                {
+                    PrintPdfInvoice(newGeneratedId);
+                }
+
             } catch (MySqlException ex)
             {
                 Console.WriteLine(ex.StackTrace);
                 MessageBox.Show("Error writing shipment request into database: \n" + ex.Message, "Database writing error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void PrintPdfInvoice(string invoiceNo)
+        {
+            Stream myStream;
+            SaveFileDialog file = new SaveFileDialog();
+            file.Filter = "pdf files (*.pdf)|*.pdf|All files (*.*)|*.*"; ;
+            file.FilterIndex = 1;
+            file.FileName = Globals.SHIPPING_INVOICE + invoiceNo;
+            file.RestoreDirectory = true;
+            var result = file.ShowDialog();
+
+            if (result.HasValue && result.Value)
+            {
+                string localFilePath = file.FileName.ToString();
+                string fileNameExt = localFilePath.Substring(localFilePath.LastIndexOf("\\") + 1);
+                myStream = file.OpenFile();
+
+                Document document = new Document(PageSize.LETTER);
+                PdfWriter writer = PdfWriter.GetInstance(document, myStream);
+                document.Open();
+
+                Font bold_big = new Font(Font.FontFamily.TIMES_ROMAN, 12, Font.BOLD | Font.UNDERLINE);
+                Font bold = new Font(Font.FontFamily.TIMES_ROMAN, 10, Font.BOLD);
+                Font normal = new Font(Font.FontFamily.TIMES_ROMAN, 10);
+
+                iTextSharp.text.Paragraph title = new iTextSharp.text.Paragraph("SHIPPING INVOICE", bold_big);
+                title.Alignment = iTextSharp.text.Rectangle.ALIGN_CENTER;
+                document.Add(title);
+
+                PdfPTable table = new PdfPTable(6);
+
+                PdfPCell cell = new PdfPCell(new Phrase(" \n "));
+                cell.Border = 0;
+                cell.Colspan = 6;
+                cell.HorizontalAlignment = 1;
+                table.AddCell(cell);
+
+                cell = new PdfPCell(new Phrase("Invoice No.", bold));
+                cell.Border = 0;
+                cell.Colspan = 1;
+                table.AddCell(cell);
+
+                cell = new PdfPCell(new Phrase(invoiceNo, normal));
+                cell.Border = 0;
+                cell.Colspan = 5;
+                table.AddCell(cell);
+
+                cell = new PdfPCell(new Phrase("Sender:", bold));
+                cell.Border = 0;
+                cell.Colspan = 3;
+                table.AddCell(cell);
+
+                cell = new PdfPCell(new Phrase("Recipient:", bold));
+                cell.Border = 0;
+                cell.Colspan = 3;
+                table.AddCell(cell);
+                // FIXED ME
+                cell = new PdfPCell(new Phrase("David Lee", normal));
+                cell.Border = 0;
+                cell.Colspan = 3;
+                table.AddCell(cell);
+                // FIXED ME
+                cell = new PdfPCell(new Phrase("Brain Jobs", normal));
+                cell.Border = 0;
+                cell.Colspan = 3;
+                table.AddCell(cell);
+
+                cell = new PdfPCell(new Phrase(shipmentRequest.Address1From + " " + shipmentRequest.Address2From, normal));
+                cell.Border = 0;
+                cell.Colspan = 3;
+                table.AddCell(cell);
+
+                cell = new PdfPCell(new Phrase(shipmentRequest.Address1To + " " + shipmentRequest.Address2To, normal));
+                cell.Border = 0;
+                cell.Colspan = 3;
+                table.AddCell(cell);
+
+                cell = new PdfPCell(new Phrase(shipmentRequest.CityFrom + " " + shipmentRequest.ProvinceFrom + ", " + shipmentRequest.CountryFrom, normal));
+                cell.Border = 0;
+                cell.Colspan = 3;
+                table.AddCell(cell);
+
+                cell = new PdfPCell(new Phrase(shipmentRequest.CityTo + " " + shipmentRequest.ProvinceTo + ", " + shipmentRequest.CountryTo, normal));
+                cell.Border = 0;
+                cell.Colspan = 3;
+                table.AddCell(cell);
+
+                cell = new PdfPCell(new Phrase(shipmentRequest.PostalCodeFrom, normal));
+                cell.Border = 0;
+                cell.Colspan = 3;
+                table.AddCell(cell);
+
+                cell = new PdfPCell(new Phrase(shipmentRequest.PostalCodeTo, normal));
+                cell.Border = 0;
+                cell.Colspan = 3;
+                table.AddCell(cell);
+
+                cell = new PdfPCell(new Phrase(" \n "));
+                cell.Border = 0;
+                cell.Colspan = 6;
+                table.AddCell(cell);
+
+                cell = new PdfPCell(new Phrase("Service Type", bold));
+                cell.Colspan = 2;
+                cell.HorizontalAlignment = 1;
+                cell.VerticalAlignment = 1;
+                table.AddCell(cell);
+
+                cell = new PdfPCell(new Phrase("Guaranteed Service", bold));
+                cell.HorizontalAlignment = 1;
+                cell.VerticalAlignment = 1;
+                table.AddCell(cell);
+
+                cell = new PdfPCell(new Phrase("Delivery Days", bold));
+                cell.HorizontalAlignment = 1;
+                cell.VerticalAlignment = 1;
+                table.AddCell(cell);
+
+                cell = new PdfPCell(new Phrase("Estimated Delivery Date", bold));
+                cell.HorizontalAlignment = 1;
+                cell.VerticalAlignment = 1;
+                table.AddCell(cell);
+
+                cell = new PdfPCell(new Phrase("Amount", bold));
+                cell.HorizontalAlignment = 1;
+                cell.VerticalAlignment = 1;
+                table.AddCell(cell);
+
+                cell = new PdfPCell(new Phrase(rate.ServiceType, normal));
+                cell.Colspan = 2;
+                cell.HorizontalAlignment = 1;
+                cell.VerticalAlignment = 1;
+                table.AddCell(cell);
+
+                cell = new PdfPCell(new Phrase(rate.Guaranteed, normal));
+                cell.HorizontalAlignment = 1;
+                cell.VerticalAlignment = 1;
+                table.AddCell(cell);
+
+                cell = new PdfPCell(new Phrase(rate.DeliverDaysStr, normal));
+                cell.HorizontalAlignment = 1;
+                cell.VerticalAlignment = 1;
+                table.AddCell(cell);
+
+                cell = new PdfPCell(new Phrase(rate.EstimatedDeliveryDateTimeStr, normal));
+                cell.HorizontalAlignment = 1;
+                cell.VerticalAlignment = 1;
+                table.AddCell(cell);
+
+                cell = new PdfPCell(new Phrase(rate.AmountStr + Globals.CURRENCY_CAD, normal));
+                cell.HorizontalAlignment = 1;
+                cell.VerticalAlignment = 1;
+                table.AddCell(cell);
+
+                cell = new PdfPCell(new Phrase(" \n \n \n \n "));
+                cell.Border = 0;
+                cell.Colspan = 6;
+                table.AddCell(cell);
+
+                cell = new PdfPCell(new Phrase("SUB TOTAL", bold));
+                cell.Border = 0;
+                cell.Colspan = 5;
+                cell.HorizontalAlignment = 2;
+                table.AddCell(cell);
+
+                cell = new PdfPCell(new Phrase(rate.AmountStr + Globals.CURRENCY_CAD, normal));
+                cell.Border = 0;
+                //cell.Colspan = 1;
+                cell.HorizontalAlignment = 2;
+                table.AddCell(cell);
+
+                cell = new PdfPCell(new Phrase("SERVICE TAX (15%)", bold));
+                cell.Border = 0;
+                cell.Colspan = 5;
+                cell.HorizontalAlignment = 2;
+                table.AddCell(cell);
+
+                double tax = rate.Amount * 0.15;
+                cell = new PdfPCell(new Phrase(string.Format("{0:0.00}{1}", tax, Globals.CURRENCY_CAD), normal));
+                cell.Border = 0;
+                //cell.Colspan = 1;
+                cell.HorizontalAlignment = 2;
+                table.AddCell(cell);
+
+                cell = new PdfPCell(new Phrase("TOTAL", bold));
+                cell.Border = 0;
+                cell.Colspan = 5;
+                cell.HorizontalAlignment = 2;
+                table.AddCell(cell);
+
+                double total = rate.Amount + tax;
+                cell = new PdfPCell(new Phrase(string.Format("{0:0.00}{1}", total, Globals.CURRENCY_CAD), normal));
+                cell.Border = 0;
+                //cell.Colspan = 1;
+                cell.HorizontalAlignment = 2;
+                table.AddCell(cell);
+
+                document.Add(table);
+
+                document.Close();
+                myStream.Close();
+
+
+                //gridBill.item
+                Grid myGrid = new Grid();
+
             }
         }
     }
